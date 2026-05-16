04a685a4a0d5a70b6e5934effed0306baccd67ff
Vadim Zhukov <zebnoid@users.noreply.github.com>
2026-05-16 07:33:35 +0500

    Row

--- Blade/Widgets/Layout/Row/Row.cpp
+++ Blade/Widgets/Layout/Row/Row.cpp
@@ -2,47 +2,53 @@
 
 auto Row::measure(const Size available) -> Size
 {
-    int totalHeight = 0;
-    int maxWidth = 0;
+    int totalWidth = 0;
+    int maxHeight = 0;
 
     for (const auto& child : m_children)
     {
         auto [width, height] = child->measure(available);
+        const auto& margin = child->layout().margin;
 
-        //  TODO
-        // const auto& margin = child->layout().margin;
-        //
-        // totalHeight +=
-        //     margin.top +
-        //     height +
-        //     margin.bottom;
-        //
-        // maxWidth = max(
-        //     maxWidth,
-        //     margin.left +
-        //     width +
-        //     margin.right
-        // );
+        totalWidth +=
+            margin.left +
+            width +
+            margin.right;
 
-        totalHeight += height;
-        maxWidth = max(maxWidth, width);
+        maxHeight = max(
+            maxHeight,
+            margin.top +
+            height +
+            margin.bottom
+        );
     }
 
     return {
-        maxWidth,
-        totalHeight
+        totalWidth,
+        maxHeight
     };
 }
 
 auto Row::arrange(const Rect rect) -> void
 {
     Widget::arrange(rect);
+
     int x = rect.x;
+    bool first = true;
 
     for (const auto& child : m_children)
     {
         auto [width, height] = child->measure({rect.width, rect.height});
         const auto& margin = child->layout().margin;
+
+        if (!first)
+        {
+            x += m_props.spacing;
+        }
+        else
+        {
+            first = false;
+        }
 
         x += margin.left;
 
