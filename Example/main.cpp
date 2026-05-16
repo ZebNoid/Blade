bcad563218d5564900fc53331a0fa7be4bb95a18
Vadim Zhukov <zebnoid@users.noreply.github.com>
2026-05-17 01:36:32 +0500

    LayoutEngine 0.1.7

--- Example/main.cpp
+++ Example/main.cpp
@@ -7,6 +7,95 @@ using namespace Blade;
 
 class Sandbox : public App
 {
+
+protected:
+    auto ui() -> void override
+    {
+        window(contentFlex()).set({
+            .title = "Flex"
+        });
+
+        window(contentBladeDemo()).id(windowId).set({
+            .title = "Blade",
+            .size = {800, 600},
+            .padding = 16,
+        });
+    }
+
+    auto contentBladeDemo() -> Column
+    {
+        return Column(
+                Label("Blade Example").id(labelId),
+                TextField("Default text").set({
+                    .layout = layoutMarginH,
+                    .textAlign = TextAlign::Start,
+                }).id(textFieldId),
+                Label("Radio Buttons").set(labelStyle),
+                Row(
+                    RadioButton("RadioButton"),
+                    RadioButton("RadioButton").set({
+                        .layout = {.flex = 1}
+                    }),
+                    RadioButton("RadioButton")
+                ).set({
+                    .gap = gap,
+                    .layout = layoutMarginH,
+                }),
+                Label("Row Buttons").set(labelStyle),
+                Row(
+                    Button("Default Button").set({
+                        .defaultButton = true,
+                    }),
+                    Button("Button"),
+                    Button("Button")
+                ).set({
+                    .gap = gap,
+                    .layout = layoutMarginH,
+                    .mainAxisAlignment = MainAxisAlignment::Center,
+                    .crossAxisAlignment = CrossAxisAlignment::Stretch,
+                }),
+                Label("Checkboxes"),
+                Row(
+                    Checkbox("Checkbox"),
+                    Checkbox("Checkbox"),
+                    Checkbox("Checkbox")
+                ).set({
+                    .layout = layoutMarginH,
+                    .mainAxisAlignment = MainAxisAlignment::Center,
+                    .crossAxisAlignment = CrossAxisAlignment::Center,
+                }),
+                Button("Button"),
+                Button("Button"),
+                Button("Button margin").set({
+                    .layout = layoutMarginH,
+                }),
+                Button("Button")
+            )
+            .set({
+                .gap = gap,
+                .mainAxisAlignment = MainAxisAlignment::Start,
+                .crossAxisAlignment = CrossAxisAlignment::Stretch,
+            });
+    }
+
+    auto contentFlex() -> Column
+    {
+        return Column(
+            Label("Header"),
+
+            Label("Content").set({
+                .layout = {
+                    .flex = 1
+                }
+            }),
+
+            Label("Footer")
+        ).set({
+            .mainAxisAlignment = MainAxisAlignment::Center,
+            .crossAxisAlignment = CrossAxisAlignment::Stretch,
+        });
+    }
+
 public:
     const int gap = 8;
 
@@ -24,107 +113,6 @@ public:
     WidgetId windowId;
     WidgetId textFieldId;
     WidgetId labelId;
-
-protected:
-    auto contentFlex() -> Widget&&
-    {
-        return Column(
-            Label("Header"),
-
-            Label("Content").set({
-                .layout = {
-                    .flex = 1
-                }
-            }),
-
-            Label("Footer")
-        );
-    }
-
-    auto ui() -> void override
-    {
-        // window(contentFlex()); // TODO fix
-
-        window(
-            Column(
-                Button("Header"),
-
-                Label("Content").set({
-                    .layout = {
-                        .flex = 1
-                    }
-                }),
-
-                Label("Footer")
-            ).set({
-                .mainAxisAlignment = MainAxisAlignment::Center,
-                .crossAxisAlignment = CrossAxisAlignment::Stretch,
-            })
-        ).set({
-            .title = "Test"
-        });
-
-        /*
-                window(
-                    Column(
-                        Label("Blade Example").id(labelId),
-                        TextField("Default text").set({
-                            .layout = layoutMarginH,
-                            .textAlign = TextAlign::Start,
-                        }).id(textFieldId),
-                        Label("Radio Buttons").set(labelStyle),
-                        Row(
-                            RadioButton("RadioButton"),
-                            RadioButton("RadioButton").set({
-                                .layout = {.flex = 1}
-                            }),
-                            RadioButton("RadioButton")
-                        ).set({
-                            .gap = gap,
-                            .layout = layoutMarginH,
-                        }),
-                        Label("Row Buttons").set(labelStyle),
-                        Row(
-                            Button("Default Button").set({
-                                .defaultButton = true,
-                            }),
-                            Button("Button"),
-                            Button("Button")
-                        ).set({
-                            .gap = gap,
-                            .layout = layoutMarginH,
-                            .mainAxisAlignment = MainAxisAlignment::Center,
-                            .crossAxisAlignment = CrossAxisAlignment::Stretch,
-                        }),
-                        Label("Checkboxes"),
-                        Row(
-                            Checkbox("Checkbox"),
-                            Checkbox("Checkbox"),
-                            Checkbox("Checkbox")
-                        ).set({
-                            .layout = layoutMarginH,
-                            .mainAxisAlignment = MainAxisAlignment::Center,
-                            .crossAxisAlignment = CrossAxisAlignment::Center,
-                        }),
-                        Button("Button"),
-                        Button("Button"),
-                        Button("Button margin").set({
-                            .layout = layoutMarginH,
-                        }),
-                        Button("Button")
-                    )
-                    .set({
-                        .gap = gap,
-                        .mainAxisAlignment = MainAxisAlignment::Start,
-                        .crossAxisAlignment = CrossAxisAlignment::Stretch,
-                    })
-                ).id(windowId).set({
-                    .title = "Blade",
-                    .size = {800, 600},
-                    .padding = 16,
-                });
-                /**/
-    }
 };
 
 
