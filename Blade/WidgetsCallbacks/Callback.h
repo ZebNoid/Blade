5370875ebf8d3976f3cc5f22428ee0a04a7306d7
Vadim Zhukov <zebnoid@users.noreply.github.com>
2026-05-18 05:01:58 +0500

    Callbacks

--- Blade/WidgetsCallbacks/Callback.h
+++ Blade/WidgetsCallbacks/Callback.h
@@ -9,4 +9,9 @@ using CallbackVoid = std::function<void()>;
 using CallbackString = std::function<void(std::string)>;
 using CallbackBool = std::function<void(bool)>;
 
+// TODO EventCallback
+template<typename... Args>
+using EventCallback = std::function<void(Args...)>;
+// EventCallback<int, int>
+
 } // namespace
