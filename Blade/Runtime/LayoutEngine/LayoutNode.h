bf918612af30472d6ba0ace1973f1dfcc9ded064
Vadim Zhukov <zebnoid@users.noreply.github.com>
2026-05-23 03:33:25 +0500

    LayoutNode

---
+++ Blade/Runtime/LayoutEngine/LayoutNode.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+
+#include "Common/LayoutProps.h"
+#include "Common/Rect.h"
+#include "Common/Size.h"
+#include "Common/Types.h"
+
+
+namespace Blade {
+
+struct LayoutNode
+{
+    Api::Id id;
+
+    Api::LayoutProps layout;
+
+    Api::Size desiredSize;
+
+    Api::Rect rect;
+
+    bool isNative = false;
+
+    std::vector<LayoutNode> children;
+};
+
+
+} // namespace
